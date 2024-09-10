5dd48c33dd6cb0327a8497b1a11bca173640c562
etaboada <etaboada@sanford.co.nz>
2024-09-11 03:34:30 +1200

    pip list isolate

--- dags/hris_dag.py
+++ dags/hris_dag.py
@@ -22,47 +22,47 @@ with DAG(
 ) as dag:
 
 
-    # Define the tasks
-    pwd_task = BashOperator(
-        task_id='pwd_task',
-        bash_command='pwd'
-    )
+    # # Define the tasks
+    # pwd_task = BashOperator(
+    #     task_id='pwd_task',
+    #     bash_command='pwd'
+    # )
 
-    # Define the tasks
-    ls_task = BashOperator(
-        task_id='ls_task',
-        bash_command='ls'
-    )
+    # # Define the tasks
+    # ls_task = BashOperator(
+    #     task_id='ls_task',
+    #     bash_command='ls'
+    # )
 
-    # Define the tasks
-    ls_airflow_task = BashOperator(
-        task_id='ls_airflow_task',
-        bash_command='ls /opt/airflow'
-    )
+    # # Define the tasks
+    # ls_airflow_task = BashOperator(
+    #     task_id='ls_airflow_task',
+    #     bash_command='ls /opt/airflow'
+    # )
 
-    # Define the tasks
-    ls_airflow_git_task = BashOperator(
-        task_id='ls_airflow_git_task',
-        bash_command='ls /opt/airflow/git'
-    )
+    # # Define the tasks
+    # ls_airflow_git_task = BashOperator(
+    #     task_id='ls_airflow_git_task',
+    #     bash_command='ls /opt/airflow/git'
+    # )
 
-    # Define the tasks
-    ls_airflow_dags_task = BashOperator(
-        task_id='ls_airflow_dags_task',
-        bash_command='ls /opt/airflow/dags'
-    )
+    # # Define the tasks
+    # ls_airflow_dags_task = BashOperator(
+    #     task_id='ls_airflow_dags_task',
+    #     bash_command='ls /opt/airflow/dags'
+    # )
 
-    # Define the tasks
-    ls_echo_dagsfolder_task = BashOperator(
-        task_id='ls_echo_dagsfolder_task',
-        bash_command='ls /opt/airflow/git/sandbt-hris.git/dags'
-    )
+    # # Define the tasks
+    # ls_echo_dagsfolder_task = BashOperator(
+    #     task_id='ls_echo_dagsfolder_task',
+    #     bash_command='ls /opt/airflow/git/sandbt-hris.git/dags'
+    # )
 
-    # Define the tasks
-    ls_cd_dagsfolder_task = BashOperator(
-        task_id='ls_cd_dagsfolder_task',
-        bash_command='cd $AIRFLOW__CORE__DAGS_FOLDER'
-    )
+    # # Define the tasks
+    # ls_cd_dagsfolder_task = BashOperator(
+    #     task_id='ls_cd_dagsfolder_task',
+    #     bash_command='cd $AIRFLOW__CORE__DAGS_FOLDER'
+    # )
 
     # Define the tasks
     ls_pip_list_task = BashOperator(
@@ -70,23 +70,24 @@ with DAG(
         bash_command='pip list'
     )
 
-    # Define the tasks
-    ls_pip_install_task = BashOperator(
-        task_id='ls_pip_install_task',
-        bash_command='pip install -r $AIRFLOW__CORE__DAGS_FOLDER/requirements.txt'
-    )
+    # # Define the tasks
+    # ls_pip_install_task = BashOperator(
+    #     task_id='ls_pip_install_task',
+    #     bash_command='pip install -r $AIRFLOW__CORE__DAGS_FOLDER/requirements.txt'
+    # )
 
-    # Define the tasks
-    cd_fabric_d365_task = BashOperator(
-        task_id='cd_fabric_d365_task',
-        bash_command='cd $AIRFLOW__CORE__DAGS_FOLDER/fabric_d365'
-    )
+    # # Define the tasks
+    # cd_fabric_d365_task = BashOperator(
+    #     task_id='cd_fabric_d365_task',
+    #     bash_command='cd $AIRFLOW__CORE__DAGS_FOLDER/fabric_d365'
+    # )
 
-    # Define the tasks
-    dbt_debug_task = BashOperator(
-        task_id='dbt_debug_task',
-        bash_command='dbt debg'
-    )
+    # # Define the tasks
+    # dbt_debug_task = BashOperator(
+    #     task_id='dbt_debug_task',
+    #     bash_command='dbt debg'
+    # )
 
     # Set the task dependencies
-    ls_airflow_dags_task >> ls_echo_dagsfolder_task >> ls_cd_dagsfolder_task >> ls_pip_list_task >> cd_fabric_d365_task >> dbt_debug_task
+    # ls_airflow_dags_task >> ls_echo_dagsfolder_task >> ls_cd_dagsfolder_task >> ls_pip_list_task >> cd_fabric_d365_task >> dbt_debug_task
+    ls_pip_list_task
