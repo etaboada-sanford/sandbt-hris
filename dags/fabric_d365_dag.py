0f057eb5f4efbd75c45df4c1f67c3c44c4ec97b6
etaboada <etaboada@sanford.co.nz>
2024-09-11 07:24:38 +1200

    dbt dag

--- dags/fabric_d365_dag.py
+++ dags/fabric_d365_dag.py
@@ -2,6 +2,8 @@ import os
 from pathlib import Path
 from datetime import datetime
 from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
+from airflow.operators.bash_operator import BashOperator
+from airflow import DAG
 
 DEFAULT_DBT_ROOT_PATH = Path(__file__).parent.parent / "dags" / "fabric_d365"
 DBT_ROOT_PATH = Path(os.getenv("DBT_ROOT_PATH", DEFAULT_DBT_ROOT_PATH))
@@ -19,4 +21,32 @@ dbt_fabric_dag = DbtDag(
     start_date=datetime(2023, 9, 10),
     catchup=False,
     dag_id="dbt_fabric_dag",
+)
+
+
+
+
+# Define the task
+dbt_fabric_task = BashOperator(
+    task_id='run_dbt_model1',
+    bash_command='dbt deps',
+    dag=dbt_fabric_dag
+)
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 5, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Create the DAG with the specified schedule interval
+dag = DAG('dbt_dag', default_args=default_args, schedule_interval=timedelta(days=1))
+# Define the dbt run command as a BashOperator
+run_dbt_model = BashOperator(
+    task_id='run_dbt_model2',
+    bash_command='dbt run',
+    dag=dag
 )
